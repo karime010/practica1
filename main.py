b48e95de51a64b658033968614427282a4db3ecf
karime010 <24308060610633@cetis61.edu.mx>
2026-04-28 18:30:51 -0600

    ultcamb28

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def main():
 
 
 class GestorTareas:
-    def __init__(self, uri: str = 'mongodb://localhost:27017/'):
+    def __init__(self, uri: str = 'mongodb+srv://karimeDB:cruzsilvaari091217@clusterkarimecruz.eb4k36a.mongodb.net/?appName=ClusterKarimeCruz'):
         """Inicializar conexión a MongoDB"""
         try:
             self.cliente = MongoClient(uri, serverSelectionTimeoutMS=5000)
